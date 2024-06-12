a1d48c27359dd38394f31962a655c169edb16e18
jacobhayward <jacobhaywardx@gmail.com>
2024-06-12 16:42:48 +0100

    home desc

--- src/App.js
+++ src/App.js
@@ -12,10 +12,9 @@ function App() {
                     <p className={"about_me"}>
                         I'm Jacob Hayward, and this is a website I have made to showcase my previous work, and the
                         projects
-                        I work on whilst looking for my next step. I enjoy learning about Computer Programming, Data Science
-                        and how the world around me works. I am working towards a career in quantitative analysis so my
-                        portfolio is mainly Machine Learning, python, and SQL projects. I am also planning on doing a
-                        finance course in the near future, to learn how to evaluate a securities intrinsic value.
+                        I work on whilst looking for my next step. I enjoy learning about Computer Programming, Data
+                        Science
+                        and how the world around me works.
                         Please get in contact if you're interested in my work or have any advice on improvements, enjoy!
                     </p>
                 </div>
